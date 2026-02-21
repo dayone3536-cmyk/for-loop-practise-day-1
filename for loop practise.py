b9e81b2242b58f88e78a752c86ea38f3f3118359
dayone3536-cmyk <dayone3536@gmail.com>
2026-02-21 17:01:01 +0530

    Add files via upload

---
+++ for loop practise.py
@@ -0,0 +1,16 @@
+'''
+START.
+give the list
+use a for loop to find how many of them are greter than 6,
+    how.,
+FIRST DECLARE A VARIABLE CALLED num IN NUMBERS 
+THEN GIVE AN IF CONDITION OF THE THINGS BEING GRETER OR NOT
+print the number
+
+'''
+count = 0
+numbers = [3, 7, 2, 9, 12, 5, 8]
+for num in numbers:
+    if num > 6:
+        count =+ count+1
+        print(count)
